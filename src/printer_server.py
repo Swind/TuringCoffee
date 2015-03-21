88a684d904826680a60ff0e191e8557afe20e690
Swind <swind.ou@ProphetStor.com>
2015-03-21 16:10:49 +0800

    Add refill server

--- src/printer_server.py
+++ src/printer_server.py
@@ -24,76 +24,80 @@ from nanomsg import (
     Socket
 )
 
-class serialComm(object):
-	"""
-	The serialComm class is the interface class which handles the communication between stdin/stdout and the machineCom class.
-	This interface class is used to run the (USB) serial communication in a different process then the GUI.
-	"""
-	def __init__(self):
-            # Read config
-            self.config = json_config.parse_json("config.json")
 
-            # Create nanomsg socket to publish status and receive command
-            self.pub_socket = Socket(PUB)
-            self.pub_socket.bind(self.config["PrinterServer"]["Publish_Socket_Address"])
+class PrinterServer(object):
 
-            # Receive the printer command
-            self.cmd_socket = Socket(PAIR)
-            self.cmd_socket.bind(self.config["PrinterServer"]["Command_Socket_Address"])
+    """
+    The serialComm class is the interface class which handles the communication between stdin/stdout and the machineCom class.
+    This interface class is used to run the (USB) serial communication in a different process then the GUI.
+    """
 
-            self._comm = None
-            self._gcodeList = []
+    def __init__(self):
+        # Read config
+        self.config = json_config.parse_json("config.json")
 
-            port_name = self.config["Printer"]["PortName"]
-            baudrate = int(self.config["Printer"]["Baudrate"])
+        # Create nanomsg socket to publish status and receive command
+        self.pub_socket = Socket(PUB)
+        self.pub_socket.bind(self.config["PrinterServer"]["Publish_Socket_Address"])
 
-            self._comm = machineCom.MachineCom(port_name, baudrate, callbackObject=self)
+        # Receive the printer command
+        self.cmd_socket = Socket(PAIR)
+        self.cmd_socket.bind(self.config["PrinterServer"]["Command_Socket_Address"])
 
-        # ================================================================================
-        #
-        #   MachineCom callback Interface
-        #
-        # ================================================================================
-	def mcLog(self, message):
-            self.pub_socket.send(msgpack.packb({"log": message}))
+        self._comm = None
+        self._gcodeList = []
 
-	def mcTempUpdate(self, temp, bedTemp, targetTemp, bedTargetTemp):
-	    # Because now the temperature is not controled by arduino
-            pass
+        port_name = self.config["Printer"]["PortName"]
+        baudrate = int(self.config["Printer"]["Baudrate"])
 
-	def mcStateChange(self, state):
-            if self._comm is None:
-                    return
+        self._comm = machineCom.MachineCom(port_name, baudrate, callbackObject=self)
 
-            self.pub_socket.send(msgpack.packb({"state": state, "state_string": self._comm.getStateString()}))
+    # ================================================================================
+    #
+    #   MachineCom callback Interface
+    #
+    # ================================================================================
+    def mcLog(self, message):
+        self.pub_socket.send(msgpack.packb({"log": message}))
 
-	def mcMessage(self, message):
-            self.pub_socket.send(msgpack.packb({"message": message}))
+    def mcTempUpdate(self, temp, bedTemp, targetTemp, bedTargetTemp):
+        # Because now the temperature is not controled by arduino
+        pass
 
-	def mcProgress(self, lineNr):
-            self.pub_socket.send(msgpack.packb({"progress": lineNr}))
+    def mcStateChange(self, state):
+        if self._comm is None:
+            return
 
-	def mcZChange(self, newZ):
-            self.pub_socket.send(msgpack.packb({"changeZ": newZ}))
+        self.pub_socket.send(msgpack.packb({"state": state, "state_string": self._comm.getStateString()}))
 
-	def monitorStdin(self):
-            while True:
-                req = self.cmd_socket.recv()
-                cmd = msgpack.unpackb(req)
+    def mcMessage(self, message):
+        self.pub_socket.send(msgpack.packb({"message": message}))
 
-                if 'STOP' in cmd:
-                    self._comm.cancelPrint()
-                    self._gcodeList = ['M110']
-                elif 'G' in cmd:
-                    self._gcodeList.append(line[1])
-                elif 'C' in cmd:
-                    self._comm.sendCommand(line[1])
-                elif 'START' in cmd:
-                    self._comm.printGCode(self._gcodeList)
+    def mcProgress(self, lineNr):
+        self.pub_socket.send(msgpack.packb({"progress": lineNr}))
+
+    def mcZChange(self, newZ):
+        self.pub_socket.send(msgpack.packb({"changeZ": newZ}))
+
+    def monitorStdin(self):
+        while True:
+            req = self.cmd_socket.recv()
+            cmd = msgpack.unpackb(req)
+
+            if 'STOP' in cmd:
+                self._comm.cancelPrint()
+                self._gcodeList = ['M110']
+            elif 'G' in cmd:
+                self._gcodeList.append(cmd["G"])
+            elif 'C' in cmd:
+                self._comm.sendCommand(cmd["C"])
+            elif 'START' in cmd:
+                self._comm.printGCode(self._gcodeList)
+
 
 def startMonitor():
-    comm = serialComm()
-    comm.monitorStdin()
+    server = PrinterServer()
+    server.monitorStdin()
 
 if __name__ == '__main__':
     startMonitor()
---
+++ src/refill_server.py
@@ -0,0 +1,84 @@
+import time
+from threading import Thread
+
+from utils import json_config
+
+import msgpack
+from nanomsg import (
+    PUB,
+    PAIR,
+    DONTWAIT,
+    Socket
+)
+
+import RPi.GPIO as GPIO
+
+class RefillServer(object):
+
+    stop = False
+
+    def __init__(self):
+        # Read Config
+        self.config = json_config.parse_json("config.json")
+
+        self.refill_pin = self.config["Refill"]["water_level_pin"]
+        self.motor_pin = self.config["Refill"]["motor_pin"]
+
+	GPIO.setmode(GPIO.BOARD)
+        GPIO.setup(self.refill_pin[0], GPIO.OUT)
+        GPIO.setup(self.refill_pin[1], GPIO.IN)
+
+        GPIO.setup(self.motor_pin, GPIO.OUT)
+
+        self.water_level_worker = Thread(target=self.__monitor_water_level)
+        self.water_level_worker.daemon = True
+
+        # Receive the pid controller command
+        self.cmd_socket = Socket(PAIR)
+        self.cmd_socket.bind(self.config["RefillServer"]["Command_Socket_Address"])
+
+    def start(self):
+        """
+        Receive command:
+        e.g
+        {
+            "Refill": "START"
+        }
+        """
+
+        # The main thread will handle the command socket
+        while(True):
+            req = self.cmd_socket.recv()
+            cmd = msgpack.unpackb(req)
+
+            if "Refill" in cmd:
+                if cmd["Refill"] == "START":
+                    self._refill_water()
+                    self.cmd_socket.send({"Refill": "OK"})
+                elif cmd["Refill"] == "STOP":
+                    self.stop = True
+                    self.cmd_socket.send({"Refill": "OK"})
+
+    def __monitor_water_level(self):
+        while True:
+            result = GPIO.input(self.refill_pin[1])
+
+            if result:
+                self.stop = True
+                break
+
+    def __refill_water(self):
+        self.water_level_worker.start()
+
+        while True:
+            if not self.stop:
+                GPIO.output(self.motor_pin, True)
+                time.sleep(0.01)
+                GPIO.output(self.motor_pin, False)
+                time.sleep(0.01)
+
+        self.stop = False
+
+if __name__ == '__main__':
+    server = RefillServer()
+    server.start()
